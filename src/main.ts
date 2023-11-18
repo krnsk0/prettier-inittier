4aed6cf882fa4dabd4819e442ceb5958c9c33d56
Jonathan Kurinsky <jkurinsky@tubi.tv>
2023-11-17 20:02:39 -0500

    main works

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import {
   CHECKING_FOR_CONFIG,
   CHECKING_FOR_PACKAGE_JSON,
   CHECK_FOR_PRETTIER,
+  CREATING_LOCAL_CONFIG,
   DETERMINE_PACKAGE_MANAGER,
+  DONE,
   ERROR,
   FOUND_CONFIG,
   FOUND_PACKAGE_JSON,
+  INSTALLED_PRETTIER,
   NO_PACKAGE_JSON,
   PACKAGE_MANAGER_FOUND,
   PRETTIER_ALREADY_INSTALLED,
@@ -15,8 +18,10 @@ import {
 } from './strings';
 import { checkForPackageJson } from './utils/checkForPackageJson';
 import { checkForPrettierInstallation } from './utils/checkForPrettierInstallation';
+import { createLocalConfig } from './utils/createLocalConfig';
 import { detectPackageManager } from './utils/detectPackageManager';
 import { doesDefaultConfigExist } from './utils/doesDefaultConfigExist';
+import { installPrettier } from './utils/installPrettier';
 import { writeDefaultConfig } from './utils/writeDefaultConfig';
 
 export const main = async () => {
@@ -27,7 +32,6 @@ export const main = async () => {
   } else {
     console.log(FOUND_CONFIG);
   }
-
   console.log(CHECKING_FOR_PACKAGE_JSON);
   try {
     if (!(await checkForPackageJson())) {
@@ -35,26 +39,24 @@ export const main = async () => {
       return;
     }
     console.log(FOUND_PACKAGE_JSON);
-
     console.log(DETERMINE_PACKAGE_MANAGER);
     const packageManager = await detectPackageManager();
-
     if (packageManager === 'unknown') {
       console.log(UNKNOWN_PACKAGE_MANAGER);
       return;
     }
     console.log(PACKAGE_MANAGER_FOUND, packageManager);
-
     console.log(CHECK_FOR_PRETTIER);
     if (await checkForPrettierInstallation()) {
       console.log(PRETTIER_ALREADY_INSTALLED);
-      return;
+    } else {
+      console.log(PRETTIER_NOT_INSTALLED);
+      await installPrettier(packageManager);
+      console.log(INSTALLED_PRETTIER);
     }
-    console.log(PRETTIER_NOT_INSTALLED);
-
-    // TODO INSTALL PRETTIER
-
-    // TODO WRITE DEFAULT CONFIG
+    console.log(CREATING_LOCAL_CONFIG);
+    await createLocalConfig();
+    console.log(DONE);
   } catch (error: unknown) {
     console.error(ERROR);
     console.error(error);
--- src/strings.ts
+++ src/strings.ts
@@ -1,12 +1,16 @@
+import { defaultConfigPath } from './constants';
+
 export const EXITING = 'Exiting...';
 
 export const ERROR = 'Hit an issue installing prettier:';
 
 export const CHECKING_FOR_CONFIG = 'Checking for prettier-inittier config';
 
-export const WRITE_CONFIG = 'Writing default prettier-inittier config';
+const EDIT_MESSAGE = `If you would like to customize the config that is written when invoking prettier-inittier, edit ${defaultConfigPath}`;
 
-export const FOUND_CONFIG = 'Found prettier-inittier config';
+export const WRITE_CONFIG = `Writing default prettier-inittier config. ${EDIT_MESSAGE}`;
+
+export const FOUND_CONFIG = `Found prettier-inittier config. ${EDIT_MESSAGE}`;
 
 export const CHECKING_FOR_PACKAGE_JSON = 'Checking for package.json';
 
@@ -28,3 +32,9 @@ export const UNKNOWN_PACKAGE_MANAGER =
   'Unknown package manager; cannot proceed';
 
 export const PACKAGE_MANAGER_FOUND = 'Package manager found:';
+
+export const INSTALLED_PRETTIER = 'Installed prettier';
+
+export const CREATING_LOCAL_CONFIG = 'Creating local prettier config';
+
+export const DONE = 'prettier-inittier complete!';
